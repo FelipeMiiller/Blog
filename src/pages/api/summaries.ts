4f231550811fedbe5dcfe0b37da9949debec29b5
Felipe <felipemiillerr@gmail.com>
2024-11-27 08:27:53 -0300

    .

--- src/pages/api/summaries.ts
+++ src/pages/api/summaries.ts
@@ -1,5 +1,8 @@
+//https://nextjs.org/docs/app/building-your-application/routing/route-handlers#cors
+
 import type { NextApiRequest, NextApiResponse } from "next"
 import { envConfigs } from "@/config"
+import { normalizeWWW } from "@/functions/url"
 import { getPostsInOrderForPublished } from "@/service/notion/posts"
 import { NotionPost, ResponseData } from "@/types"
 
@@ -17,12 +20,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     res.status(404).json({ message: "Not found", status: 404 })
     return
   }
-  return new Response(JSON.stringify({ data: posts, message: "Success", status: 200 }), {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": envConfigs.site.baseUrl,
+
+  res
+    .status(200)
+    .writeHead(200, "Success", {
+      "Access-Control-Allow-Origin":
+        envConfigs.site.baseUrl || normalizeWWW(envConfigs.site.baseUrl) || "http://localhost:3000",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  })
+    })
+    .json({ data: posts, message: "Success", status: 200 })
 }
